3fafb1d1dd7f07c9c885eeadae39381770f81fea
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-10 10:58:24 +0200

    Added getters, setters, constructors and relationship

--- src/main/java/se/lexicon/todoapi/domain/entity/Person.java
+++ src/main/java/se/lexicon/todoapi/domain/entity/Person.java
@@ -1,4 +1,37 @@
 package se.lexicon.todoapi.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "email")
+    private User user;
+
+    @OneToMany(mappedBy = "person")
+    private List<Task> tasks = new ArrayList<>();
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+
 }
--- src/main/java/se/lexicon/todoapi/domain/entity/Task.java
+++ src/main/java/se/lexicon/todoapi/domain/entity/Task.java
@@ -1,4 +1,31 @@
 package se.lexicon.todoapi.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate deadline;
+    private boolean done;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
 }
