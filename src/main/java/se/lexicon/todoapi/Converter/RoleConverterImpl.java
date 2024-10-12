ebc0db9ae40903bb49d89d1ec9cacf243edd1630
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-12 11:21:29 +0200

    Implemented

--- src/main/java/se/lexicon/todoapi/Converter/RoleConverterImpl.java
+++ src/main/java/se/lexicon/todoapi/Converter/RoleConverterImpl.java
@@ -1,4 +1,23 @@
 package se.lexicon.todoapi.Converter;
 
-public class RoleConverterImpl {
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.RoleDTOForm;
+import se.lexicon.todoapi.domain.dto.RoleDTOView;
+import se.lexicon.todoapi.domain.entity.Role;
+
+@Component
+public class RoleConverterImpl implements RoleConverter{
+
+
+    @Override
+    public RoleDTOView toRoleDTO(Role entity) {
+        return RoleDTOView.builder().id(entity.getId()).name(entity.getName()).build();
+        // return new RoleDTOView(entity.getId(), entity.getName());
+    }
+
+    @Override
+    public Role toEntity(RoleDTOForm dto) {
+        return Role.builder().id(dto.getId()).name(dto.getName()).build();
+        // return new Role(dto.getId(), dto.getName());
+    }
 }
