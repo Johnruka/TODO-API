9be0f9166a09d392b9ed3b90a2b9459acca27642
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-12 15:22:46 +0200

    added methods

--- src/main/java/se/lexicon/todoapi/Converter/TaskConverter.java
+++ src/main/java/se/lexicon/todoapi/Converter/TaskConverter.java
@@ -1,4 +1,24 @@
 package se.lexicon.todoapi.Converter;
 
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.TaskDTOForm;
+import se.lexicon.todoapi.domain.dto.TaskDTOView;
+import se.lexicon.todoapi.domain.entity.Task;
+
+@Component
 public interface TaskConverter {
+
+    Task toTaskSave(TaskDTOForm dto);
+
+    Task toTaskForm(TaskDTOForm dto);
+
+    TaskDTOView toTaskDTOView(Task entity);
+
+    TaskDTOView toTaskDTOViewWithoutPerson(Task entity);
+
+    TaskDTOView toTaskDTOViewForm(TaskDTOForm dto);
+
+    TaskDTOForm toTskDTOForm(TaskDTOForm dto);
+
+    TaskDTOForm toTaskDTOForm(Task entity);
 }
