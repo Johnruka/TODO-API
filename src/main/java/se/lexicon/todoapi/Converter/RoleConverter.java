bdef20371db7e161e9c2eb98b90f70107d78fca4
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-12 11:19:35 +0200

    Added methods

--- src/main/java/se/lexicon/todoapi/Converter/RoleConverter.java
+++ src/main/java/se/lexicon/todoapi/Converter/RoleConverter.java
@@ -1,4 +1,14 @@
 package se.lexicon.todoapi.Converter;
 
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.RoleDTOForm;
+import se.lexicon.todoapi.domain.dto.RoleDTOView;
+import se.lexicon.todoapi.domain.entity.Role;
+
+@Component
 public interface RoleConverter {
+
+    RoleDTOView toRoleDTO(Role entity);
+
+    Role toEntity(RoleDTOForm dto);
 }
