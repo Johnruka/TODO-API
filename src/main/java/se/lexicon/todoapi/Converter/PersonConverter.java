a99b529f55d9201a8cc0f686eede38a31a5f36bc
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-12 15:32:03 +0200

    added methods

--- src/main/java/se/lexicon/todoapi/Converter/PersonConverter.java
+++ src/main/java/se/lexicon/todoapi/Converter/PersonConverter.java
@@ -1,4 +1,22 @@
 package se.lexicon.todoapi.Converter;
 
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.PersonDTOForm;
+import se.lexicon.todoapi.domain.dto.PersonDTOView;
+import se.lexicon.todoapi.domain.entity.Person;
+
+@Component
 public interface PersonConverter {
+
+    Person toPersonEntitySave(PersonDTOForm dtoFormSave);
+
+    Person toPersonEntityForm(PersonDTOForm dtoForm);
+
+    PersonDTOView toPersonDTOView(Person entity);
+
+    PersonDTOView toPersonDTOView(PersonDTOForm dto);
+
+    PersonDTOForm toPersonDTOForm(PersonDTOView entity);
+
+    PersonDTOForm toPersonDTOFormEntity(Person entity);
 }
