d339b782b5450e763e3cc51c9ac34c5d1f2b39ff
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-12 10:33:50 +0200

    created and implemented

---
+++ src/main/java/se/lexicon/todoapi/util/CustomPasswordEncoder.java
@@ -0,0 +1,19 @@
+package se.lexicon.todoapi.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
