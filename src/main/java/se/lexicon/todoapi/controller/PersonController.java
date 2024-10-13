c49bbe54933b6d908bc7151a74635538099177b4
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 15:25:31 +0200

    added constructors

--- src/main/java/se/lexicon/todoapi/controller/PersonController.java
+++ src/main/java/se/lexicon/todoapi/controller/PersonController.java
@@ -1,4 +1,18 @@
 package se.lexicon.todoapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.todoapi.Service.PersonService;
+
+@RequestMapping("/api/v1/person")
+@RestController
 public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 }
--- src/main/java/se/lexicon/todoapi/controller/TaskController.java
+++ src/main/java/se/lexicon/todoapi/controller/TaskController.java
@@ -1,4 +1,18 @@
 package se.lexicon.todoapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.todoapi.Service.TaskService;
+
+@RequestMapping("/api/v1/tasks")
+@RestController
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 }
