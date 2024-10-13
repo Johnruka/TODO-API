4ab8b041dd531ae4b4d1e8fb81bb0ad85033c9fd
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 13:49:41 +0200

    Implemented

--- src/main/java/se/lexicon/todoapi/Converter/PersonConverterImpl.java
+++ src/main/java/se/lexicon/todoapi/Converter/PersonConverterImpl.java
@@ -1,4 +1,26 @@
 package se.lexicon.todoapi.Converter;
 
-public class PersonConverterImpl {
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.PersonDTOForm;
+import se.lexicon.todoapi.domain.dto.PersonDTOView;
+import se.lexicon.todoapi.domain.entity.Person;
+
+@Component
+public class PersonConverterImpl implements PersonConverter {
+
+
+    @Override
+    public PersonDTOView toPersonDTO(PersonDTOView entity) {
+        return PersonDTOView.builder().id(entity.getId()).name(entity.getName()).build();
+        // return new PersonDTOView(entity.getId(), entity.getName());
+    }
+
+    @Override
+    public Person toEntity(PersonDTOForm dto) {
+        return Person.builder().id(dto.getId()).name(dto.getName()).build();
+        // return new Person(dto.getId(), dto.getName());
+    }
 }
+
+
+
