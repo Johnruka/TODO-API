ab61f704929766231c937126d067c20fbf632b36
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 15:09:45 +0200

    Implemented

--- src/main/java/se/lexicon/todoapi/controller/RoleController.java
+++ src/main/java/se/lexicon/todoapi/controller/RoleController.java
@@ -1,4 +1,29 @@
 package se.lexicon.todoapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.todoapi.Service.RoleService;
+import se.lexicon.todoapi.domain.dto.RoleDTOView;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/roles")
+@RestController
 public class RoleController {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleDTOView>> doGetAllRoles() {
+        List<RoleDTOView> responseBody = roleService.getAll();
+        return ResponseEntity.ok(responseBody);
+    }
 }
