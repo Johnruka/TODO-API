a218922f5f6d5a1309d82b99d87d34562db2b514
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 14:25:02 +0200

    Implemened

--- src/main/java/se/lexicon/todoapi/Converter/UserConverterImpl.java
+++ src/main/java/se/lexicon/todoapi/Converter/UserConverterImpl.java
@@ -1,4 +1,33 @@
 package se.lexicon.todoapi.Converter;
 
-public class UserConverterImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.domain.dto.UserDTOForm;
+import se.lexicon.todoapi.domain.dto.UserDTOView;
+import se.lexicon.todoapi.domain.entity.User;
+import se.lexicon.todoapi.util.CustomPasswordEncoder;
+
+@Component
+public class UserConverterImpl implements UserConverter {
+
+    RoleConverter roleConverter;
+    CustomPasswordEncoder customPasswordEncoder;
+
+    @Autowired
+    public UserConverterImpl(RoleConverter roleConverter, CustomPasswordEncoder customPasswordEncoder) {
+        this.roleConverter = roleConverter;
+        this.customPasswordEncoder = customPasswordEncoder;
+    }
+
+
+    @Override
+    public UserDTOView toUserDTO(User entity) {
+        return UserDTOView.builder().email(entity.getEmail()).build();
+
+    }
+
+    @Override
+    public User toEntity(UserDTOForm dto) {
+        return User.builder().email(dto.getEmail()).build();
+    }
 }
