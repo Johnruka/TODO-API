e8ef321386c1e7c2bb073dec52c8b94d21b27393
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 18:26:30 +0200

    Implemented

--- src/test/java/se/lexicon/todoapi/service/UserServiceImplTest.java
+++ src/test/java/se/lexicon/todoapi/service/UserServiceImplTest.java
@@ -1,4 +1,30 @@
 package se.lexicon.todoapi.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.lexicon.todoapi.Repository.RoleRepository;
+import se.lexicon.todoapi.Repository.UserRepository;
+import se.lexicon.todoapi.Service.Impl.UserServiceImpl;
+import se.lexicon.todoapi.util.CustomPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private RoleRepository roleRepository;
+    @Mock
+    private CustomPasswordEncoder passwordEncoder;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
 }
