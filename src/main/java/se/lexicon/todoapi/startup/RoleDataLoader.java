06a9d593ff263b0dd961d79e66147f01930d853f
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-13 14:43:21 +0200

    implemented

--- src/main/java/se/lexicon/todoapi/startup/RoleDataLoader.java
+++ src/main/java/se/lexicon/todoapi/startup/RoleDataLoader.java
@@ -1,4 +1,27 @@
 package se.lexicon.todoapi.startup;
 
-public class RoleDataLoader {
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import se.lexicon.todoapi.Repository.RoleRepository;
+import se.lexicon.todoapi.domain.entity.Role;
+
+@Component
+public class RoleDataLoader implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleDataLoader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+        public void run (String...args) throws Exception {
+
+        System.out.println("#####################################");
+        roleRepository.save(new Role("ADMIN"));
+        roleRepository.save(new Role("USER"));
+        roleRepository.save(new Role("GUEST"));
+        }
+    }
